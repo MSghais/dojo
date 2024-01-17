da4ae95155bb4fdd8749c1db27d7f0a124ddd827
Kariy <ammr.arf@gmail.com>
2024-01-17 11:08:27 +0900

    wip

--- crates/katana/executor/src/lib.rs
+++ crates/katana/executor/src/lib.rs
@@ -1,1 +1,22 @@
+use katana_primitives::block::Block;
+use katana_primitives::state::StateUpdatesWithDeclaredClasses;
+
 pub mod blockifier;
+
+pub trait StarknetProcessor {
+    fn execute(&self);
+    fn execute_many(&self);
+    fn estimate_fee(&self);
+    fn call(&self);
+}
+
+pub trait BlockExecutor: StarknetProcessor {
+    fn execute_block(&mut self, block: &Block);
+    fn take_output(&mut self) -> StateUpdatesWithDeclaredClasses;
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test() {}
+}
